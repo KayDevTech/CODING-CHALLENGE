63b9f6a48bda8327031adaa10194bbc5944ddfc2
Caleb Mensah <kay.tech.adv@gmail.com>
2025-05-15 00:40:08 +0000

    Building the UI

---
+++ src/containers/Header.tsx
@@ -0,0 +1,13 @@
+import type { FC } from "react";
+
+
+const Header: FC =() => {
+    return(
+        <div>
+            <div>
+                <h2>Fake Shop</h2>
+            </div>
+        </div>
+    )
+}
+export default Header
